8c9c7e3a4ed3c204e2807745c89a5228abd24513
Konstantin Taletskiy <konstantin@taletskiy.com>
2021-12-15 10:46:32 -0800

    feat: Update test_client to use Keycloak auth

--- wipp_client/test/test_client.py
+++ wipp_client/test/test_client.py
@@ -1,90 +1,152 @@
-from wipp_client import Wipp, WippPlugin
+# Test script
+# Demonstrates examples of client use
+# Requires python-keycloak to be installed (`pip install python-keycloak`)
+# Please fill in the relevant `keycloak_server_url`, `username`, `password`
 
+from keycloak import KeycloakOpenID
+from wipp_client import (
+    Wipp,
+    WippImageCollection,
+    WippCsvCollection,
+    WippGenericDataCollection,
+    WippPlugin,
+)
+
+
+# Configure Keycloak client
+keycloak_openid = KeycloakOpenID(
+    server_url="<keycloak_server_url>/auth/",
+    client_id="wipp-public-client",
+    realm_name="WIPP",
+)
+
+# Get Token
+token = keycloak_openid.token("<username>", "<password>")
+
+# Test that token is valid in Keycloak by getting some information about the user
+userinfo = keycloak_openid.userinfo(token["access_token"])
+print(f"Keycloak user: {userinfo['name']}")
+
+
+# Create WIPP client
 w = Wipp()
 
-# Search image collections
-collections = w.search_image_collections("ratBrain")
-for c in collections:
+# Configure Keycloak token in the client
+w.auth_headers = token["access_token"]
+
+### Test client functionality ###
+
+# Create Image Collection
+ic = WippImageCollection(name="Test Image collection")
+nic = w.create_image_collection(ic)
+print(f"Created Image Collection: {nic.id}")
+
+for c in w.get_image_collections():
     print(c)
 
-# Get list of images in the image collection
-images = w.get_image_collections_images(collections[0].id)
-for i in images:
-    print(i)
+print(f"Deleted Image Collection: {nic.id}")
+w.delete_image_collection(nic.id)
 
-# Search CSV collections
-csv_collections = w.search_csv_collections("covid")
-for csvc in csv_collections:
-    print(csvc)
+for c in w.get_image_collections():
+    print(c)
 
-# Get list of CSV files in the CSV collection
-w.get_csv_collections_csv_files(csv_collections[0].id)
+# Create CSV collection
+csv_collection = WippCsvCollection(name="Test CSV collection")
+created_csv_collection = w.create_csv_collection(csv_collection)
+
+print(f"Created CSV Collection: {created_csv_collection.id}")
+
+for c in w.get_csv_collections():
+    print(c)
+
+print(f"Deleted CSV Collection: {created_csv_collection.id}")
+w.delete_csv_collection(created_csv_collection.id)
+
+for c in w.get_csv_collections():
+    print(c)
+
+# Create Generic Data Collection (does not work in API yet)
+# generic_data_collection = WippGenericDataCollection(name = "Test Generic Data Collection")
+
+# created_generic_data_collection = w.create_generic_data_collection(generic_data_collection)
+
+# print(created_generic_data_collection.id)
+
+# print(w.get_generic_data_collections())
+
+# w.delete_generic_data_collection(created_generic_data_collection.id)
+
+# print(w.get_generic_data_collections())
 
 # Crerate plugin object
 plugin = {
-  "name": "WIPP API Test",
-  "version": "0.0.0",
-  "containerId": "ktaletsk/noop",
-  "title": "Test Plugin Created by Python WIPP API client 0.2.0",
-  "description": "This plugin does nothing",
-  "author": "Konstantin Taletskiy (konstantin.taletskiy@labshare.org)",
-  "institution": "National Center for Advancing Translational Sciences, National Institutes of Health",
-  "repository": "https://github.com/labshare/polus-plugins",
-  "website": "",
-  "citation": "",
-  "inputs": [
-    {
-      "name": "inpImageDir",
-      "description": "Input Image collection to make predictions on",
-      "type": "collection",
-      "required": True
-    },
-    {
-      "name": "inpBaseDir",
-      "description": "Input SplineDist Model that contains weights",
-      "type": "genericData",
-      "required": True
-    },
-    {
-      "name": "imagePattern",
-      "description": "Pattern of the images in Input",
-      "type": "string",
-      "required": False
-    }
-  ],
-  "outputs": [
-    {
-      "name": "outDir",
-      "description": "Output Directory for Predicted Images",
-      "type": "genericData",
-      "required": True
-    }
-  ],
-  "ui": [
-    {
-      "key": "inputs.inpImageDir",
-      "title": "Input Image Directory: ",
-      "description": "Collection name that contains intensity based images"
-    },
-    {
-      "key": "inputs.inpBaseDir",
-      "title": "Model Directory: ",
-      "description": "Directory containing the model weights and config file"
-    },
-    {
-      "key": "inputs.imagePattern",
-      "title": "Image Pattern: ",
-      "description": "Pattern of images in input collection (image_r{rrr}_c{ccc}_z{zzz}.ome.tif). "
-    }
-  ]
+    "name": "WIPP API Test",
+    "version": "0.0.5",
+    "containerId": "ktaletsk/noop",
+    "title": "Test Plugin Created by Python WIPP API client 0.0.5",
+    "description": "This plugin does nothing",
+    "author": "Konstantin Taletskiy (konstantin.taletskiy@labshare.org)",
+    "institution": "National Center for Advancing Translational Sciences, National Institutes of Health",
+    "repository": "https://github.com/labshare/polus-plugins",
+    "website": "",
+    "citation": "",
+    "inputs": [
+        {
+            "name": "inpImageDir",
+            "description": "Input Image collection to make predictions on",
+            "type": "collection",
+            "required": True,
+        },
+        {
+            "name": "inpBaseDir",
+            "description": "Input SplineDist Model that contains weights",
+            "type": "genericData",
+            "required": True,
+        },
+        {
+            "name": "imagePattern",
+            "description": "Pattern of the images in Input",
+            "type": "string",
+            "required": False,
+        },
+    ],
+    "outputs": [
+        {
+            "name": "outDir",
+            "description": "Output Directory for Predicted Images",
+            "type": "genericData",
+            "required": True,
+        }
+    ],
+    "ui": [
+        {
+            "key": "inputs.inpImageDir",
+            "title": "Input Image Directory: ",
+            "description": "Collection name that contains intensity based images",
+        },
+        {
+            "key": "inputs.inpBaseDir",
+            "title": "Model Directory: ",
+            "description": "Directory containing the model weights and config file",
+        },
+        {
+            "key": "inputs.imagePattern",
+            "title": "Image Pattern: ",
+            "description": "Pattern of images in input collection (image_r{rrr}_c{ccc}_z{zzz}.ome.tif). ",
+        },
+    ],
 }
 
-wp = WippPlugin(plugin)
-print(wp)
+wp = WippPlugin(**plugin)
 
 # Register plugin
 np = w.create_plugin(wp)
-print(np.id)
+print(np)
+print(f"Registered plugin: {np.id}")
+
+for p in w.get_plugins():
+    print(p)
 
 # Delete plugin
 w.delete_plugin(np.id)
+print(f"Deleted plugin: {np.id}")
